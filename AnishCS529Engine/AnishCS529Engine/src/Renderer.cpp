674297f35c7be4543d507e78b35dd219b1e47165
Anish Murthy <anish.murthy@digipen.edu>
2024-10-02 12:07:58 -0700

    Upgraded glad to OpenGL4.3 and reordered files

--- AnishCS529Engine/AnishCS529Engine/src/Renderer.cpp
+++ AnishCS529Engine/AnishCS529Engine/src/Renderer.cpp
@@ -2,6 +2,53 @@
 
 const float pi = 3.14159f;
 
+// OpenGL Debugging function
+void APIENTRY openglCallbackFunction(
+	GLenum source, GLenum type, GLuint id, GLenum severity,
+	GLsizei length, const GLchar* message, const void* userParam)
+{
+	std::string sourceStr;
+	switch (source)
+	{
+	case GL_DEBUG_SOURCE_API:             sourceStr = "API"; break;
+	case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   sourceStr = "Window System"; break;
+	case GL_DEBUG_SOURCE_SHADER_COMPILER: sourceStr = "Shader Compiler"; break;
+	case GL_DEBUG_SOURCE_THIRD_PARTY:     sourceStr = "Third Party"; break;
+	case GL_DEBUG_SOURCE_APPLICATION:     sourceStr = "Application"; break;
+	case GL_DEBUG_SOURCE_OTHER:           sourceStr = "Other"; break;
+	}
+
+	std::string typeStr;
+	switch (type)
+	{
+	case GL_DEBUG_TYPE_ERROR:               typeStr = "Error"; break;
+	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: typeStr = "Deprecated Behavior"; break;
+	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  typeStr = "Undefined Behavior"; break;
+	case GL_DEBUG_TYPE_PORTABILITY:         typeStr = "Portability"; break;
+	case GL_DEBUG_TYPE_PERFORMANCE:         typeStr = "Performance"; break;
+	case GL_DEBUG_TYPE_MARKER:              typeStr = "Marker"; break;
+	case GL_DEBUG_TYPE_PUSH_GROUP:          typeStr = "Push Group"; break;
+	case GL_DEBUG_TYPE_POP_GROUP:           typeStr = "Pop Group"; break;
+	case GL_DEBUG_TYPE_OTHER:               typeStr = "Other"; break;
+	}
+
+	std::string severityStr;
+	switch (severity)
+	{
+	case GL_DEBUG_SEVERITY_HIGH:         severityStr = "High"; break;
+	case GL_DEBUG_SEVERITY_MEDIUM:       severityStr = "Medium"; break;
+	case GL_DEBUG_SEVERITY_LOW:          severityStr = "Low"; break;
+	case GL_DEBUG_SEVERITY_NOTIFICATION: severityStr = "Notification"; break;
+	}
+
+	std::cerr << "OpenGL Debug Message\n"
+		<< "Source: " << sourceStr << "\n"
+		<< "Type: " << typeStr << "\n"
+		<< "ID: " << id << "\n"
+		<< "Severity: " << severityStr << "\n"
+		<< "Message: " << message << std::endl;
+}
+
 /* Private functions */
 void Renderer::initialize() {
 	gameWindow.setWindowHints([]() {
@@ -25,6 +72,10 @@ bool Renderer::loadGraphicsAPIFunctions() {
 	{
 		return false;
 	}
+	// Enable debug
+	glEnable(GL_DEBUG_OUTPUT);
+	glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS); // Makes sure errors are reported immediately
+	glDebugMessageCallback(openglCallbackFunction, nullptr);
 	return true;
 }
 
