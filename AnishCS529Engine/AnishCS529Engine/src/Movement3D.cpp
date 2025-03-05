8bb228ce175a20f1c2227f464a580755894df6c7
hobbes144 <77174374+hobbes144@users.noreply.github.com>
2025-03-04 18:20:46 -0800

    patch movement3d

--- AnishCS529Engine/AnishCS529Engine/src/Movement3D.cpp
+++ AnishCS529Engine/AnishCS529Engine/src/Movement3D.cpp
@@ -87,7 +87,7 @@ std::shared_ptr<Movement3D> Movement3D::setAction(Action action, Key key)
   case RollClockwise:
     actionFunction = [this, action, key](std::shared_ptr<PhysicsBody> physicsBody) {
       if ( inputSystem->isKeyDown(key) ) {
-        physicsBody->applyRotationalForce(Vector3(0.0f, 0.0f, -force));
+        physicsBody->applyRotationalForce(Vector3(0.0f, 0.0f, force));
         Movement3DEvent moveEvent(this->getParent(), action);
         EventManager::Instance().BroadcastEvent(moveEvent);
       } };
@@ -95,7 +95,7 @@ std::shared_ptr<Movement3D> Movement3D::setAction(Action action, Key key)
   case RollAntiClockwise:
     actionFunction = [this, action, key](std::shared_ptr<PhysicsBody> physicsBody) {
       if ( inputSystem->isKeyDown(key) ) {
-        physicsBody->applyRotationalForce(Vector3(0.0f, 0.0f, force));
+        physicsBody->applyRotationalForce(Vector3(0.0f, 0.0f, -force));
         Movement3DEvent moveEvent(this->getParent(), action);
         EventManager::Instance().BroadcastEvent(moveEvent);
       } };
@@ -103,7 +103,7 @@ std::shared_ptr<Movement3D> Movement3D::setAction(Action action, Key key)
   case PitchClockwise:
     actionFunction = [this, action, key](std::shared_ptr<PhysicsBody> physicsBody) {
       if ( inputSystem->isKeyDown(key) ) {
-        physicsBody->applyRotationalForce(Vector3(-force, 0.0f, 0.0f));
+        physicsBody->applyRotationalForce(Vector3(force, 0.0f, 0.0f));
         Movement3DEvent moveEvent(this->getParent(), action);
         EventManager::Instance().BroadcastEvent(moveEvent);
       } };
@@ -111,7 +111,7 @@ std::shared_ptr<Movement3D> Movement3D::setAction(Action action, Key key)
   case PitchAnticlockwise:
     actionFunction = [this, action, key](std::shared_ptr<PhysicsBody> physicsBody) {
       if ( inputSystem->isKeyDown(key) ) {
-        physicsBody->applyRotationalForce(Vector3(force, 0.0f, 0.0f));
+        physicsBody->applyRotationalForce(Vector3(-force, 0.0f, 0.0f));
         Movement3DEvent moveEvent(this->getParent(), action);
         EventManager::Instance().BroadcastEvent(moveEvent);
       } };
@@ -119,7 +119,7 @@ std::shared_ptr<Movement3D> Movement3D::setAction(Action action, Key key)
   case YawClockwise:
     actionFunction = [this, action, key](std::shared_ptr<PhysicsBody> physicsBody) {
       if ( inputSystem->isKeyDown(key) ) {
-        physicsBody->applyRotationalForce(Vector3(0.0f, -force, 0.0f));
+        physicsBody->applyRotationalForce(Vector3(0.0f, force, 0.0f));
         Movement3DEvent moveEvent(this->getParent(), action);
         EventManager::Instance().BroadcastEvent(moveEvent);
       } };
@@ -127,7 +127,7 @@ std::shared_ptr<Movement3D> Movement3D::setAction(Action action, Key key)
   case YawAntiClockwise:
     actionFunction = [this, action, key](std::shared_ptr<PhysicsBody> physicsBody) {
       if ( inputSystem->isKeyDown(key) ) {
-        physicsBody->applyRotationalForce(Vector3(0.0f, force, 0.0f));
+        physicsBody->applyRotationalForce(Vector3(0.0f, -force, 0.0f));
         Movement3DEvent moveEvent(this->getParent(), action);
         EventManager::Instance().BroadcastEvent(moveEvent);
       } };
