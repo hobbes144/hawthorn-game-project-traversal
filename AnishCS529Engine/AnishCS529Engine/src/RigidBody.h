7d0d8e020b2846c4b5116ad6b47fe88995556357
KeitherJinn <80877899+KeitherJinn@users.noreply.github.com>
2025-03-05 20:35:49 -0800

    Update RigidBody.h

--- AnishCS529Engine/AnishCS529Engine/src/RigidBody.h
+++ AnishCS529Engine/AnishCS529Engine/src/RigidBody.h
@@ -22,6 +22,12 @@
 #include "GameObject.h"
 #include "Shape.h"
 #include "Transform.h"
+#include "PhysicsManager.h"
+#include <cmath>
+#include <algorithm>
+#include "AABB.h"
+#include "OBB.h"
+#include "Sphere.h"
 
 /*!****************************************************************************
  * \brief RigidBody class that implements a rigidbody component that can be 
@@ -54,6 +60,7 @@ public:
 	std::shared_ptr<RigidBody> freezingRotationX(bool value);
 	std::shared_ptr<RigidBody> freezingRotationY(bool value);
 	std::shared_ptr<RigidBody> freezingRotationZ(bool value);
+	
 
 private:
 	/** If it uses gravity */
@@ -68,12 +75,12 @@ private:
 	bool freezeRotationX;
 	bool freezeRotationY;
 	bool freezeRotationZ;
+
 };
 
 void onRBCollide(std::shared_ptr<GameObject> obj1,
 	std::shared_ptr<GameObject> obj2, const Vector3& point);
+Vector3 OBBandOBB(const std::shared_ptr<OBB> boxA, 
+	const std::shared_ptr<OBB> boxB);
 
 #endif // RIGIDBODY_H
-
-
-
