815a9bc6ab711512e3efd8593bd47375a191edba
KeitherJinn <80877899+KeitherJinn@users.noreply.github.com>
2025-03-27 14:42:11 -0700

    joystick sensitivity update

--- AnishCS529Engine/AnishCS529Engine/src/GamePad.h
+++ AnishCS529Engine/AnishCS529Engine/src/GamePad.h
@@ -22,15 +22,20 @@ private:
 	void registerKey(WORD);
 
 public:
-	GamePad() : deadzoneX(0.02f), deadzoneY(0.02f),gpXSensitivity(2.0f),gpYSensitivity(2.0f) {}
+	GamePad() : deadzoneX(0.02f), deadzoneY(0.02f),
+		gpLXSensitivity(2.0f), gpLYSensitivity(2.0f),
+		gpRXSensitivity(2.0f), gpRYSensitivity(2.0f) {
+	}
 	GamePad(float dzX, float dzY) : deadzoneX(dzX), deadzoneY(dzY) {}
 
 	float leftStickX;
 	float leftStickY;
 	float rightStickX;
 	float rightStickY;
-	float gpXSensitivity;
-	float gpYSensitivity;
+	float gpLXSensitivity;
+	float gpLYSensitivity;
+	float gpRXSensitivity;
+	float gpRYSensitivity;
 	float leftTrigger;
 	float rightTrigger;
 	std::unordered_map<WORD, GPKeyState> keyStates;
