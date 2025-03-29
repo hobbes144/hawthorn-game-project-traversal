9771ef4c036d5921cca2e6fda59a641a66ea11b7
lixiaok <lixiaok@uw.edu>
2025-03-29 01:06:05 -0700

    Tutorial Map

--- AnishCS529Engine/AnishCS529Engine/src/MapLoader.cpp
+++ AnishCS529Engine/AnishCS529Engine/src/MapLoader.cpp
@@ -66,31 +66,261 @@ void MapLoader::loadMap(int mapId, float offsetX, float offsetY, float offsetZ,
 
 
 void MapLoader::zero(float offsetX, float offsetY, float offsetZ, SceneGraph& sceneGraph, std::shared_ptr<Camera> camera, std::shared_ptr<Mesh> floorMesh, std::shared_ptr<Material> floorMaterial){
-    // Floor pad
+    
+    // Left and Right wall
+    //{
+    //    auto LeftWall = std::make_shared<GameObject>("LeftWall", GameObject::WALL);
+    //    sceneGraph.addNode(LeftWall);
+    //    LeftWall->setLocalPosition(Vector3(-200.0f + offsetX, 4.0f + offsetY, 10.1f + offsetZ));
+    //    LeftWall->setLocalScaling(Vector3(400.0f, 12.0f, 1.0f));
+    //    auto renderComp = LeftWall->addComponent<Render2D>();
+    //    renderComp->setCamera(camera)->setMesh(floorMesh)->setMaterial(floorMaterial);
+    //    auto shape = std::make_shared<OBB>();
+    //    auto rigidBody = LeftWall->addComponent<RigidBody>();
+    //    rigidBody->setMass(0.0f)
+    //        ->setDrag(1.0f)
+    //        ->setShape(shape)
+    //        ->setStatic(true)
+    //        ->registerToPhysicsManager(PhysicsManager::Instance());
+    //    rigidBody->initialize();
+    //}
+
+    //{
+    //    auto RightWall = std::make_shared<GameObject>("RightWall", GameObject::WALL);
+    //    sceneGraph.addNode(RightWall);
+    //    RightWall->setLocalPosition(Vector3(-200.0f + offsetX, 4.0f + offsetY, -10.1f + offsetZ));
+    //    RightWall->setLocalScaling(Vector3(400.0f, 12.0f, 1.0f));
+    //    auto renderComp = RightWall->addComponent<Render2D>();
+    //    renderComp->setCamera(camera)->setMesh(floorMesh)->setMaterial(floorMaterial);
+    //    auto shape = std::make_shared<OBB>();
+    //    auto rigidBody = RightWall->addComponent<RigidBody>();
+    //    rigidBody->setMass(0.0f)
+    //        ->setDrag(1.0f)
+    //        ->setShape(shape)
+    //        ->setStatic(true)
+    //        ->registerToPhysicsManager(PhysicsManager::Instance());
+    //    rigidBody->initialize();
+    //}
+    // Floor1 pad
     {
-        auto mainFloorLeft = std::make_shared<GameObject>("MainFloorLeft");
-        sceneGraph.addNode(mainFloorLeft);
-        mainFloorLeft->setLocalPosition(Vector3(-4.0f + offsetX, 0.0f + offsetY, 0.0f + offsetZ));
-        mainFloorLeft->setLocalScaling(Vector3(60.0f, 1.0f, 60.0f));
-        auto renderComp = mainFloorLeft->addComponent<Render2D>();
+        auto Floor1 = std::make_shared<GameObject>("Floor1");
+        sceneGraph.addNode(Floor1);
+        Floor1->setLocalPosition(Vector3(-20.0f + offsetX, 0.0f + offsetY, 0.0f + offsetZ));
+        Floor1->setLocalScaling(Vector3(60.0f, 1.0f, 20.0f));
+        auto renderComp = Floor1->addComponent<Render2D>();
         renderComp->setCamera(camera)->setMesh(floorMesh)->setMaterial(floorMaterial);
         auto shape = std::make_shared<OBB>();
-        auto rigidBody = mainFloorLeft->addComponent<RigidBody>();
+        auto rigidBody = Floor1->addComponent<RigidBody>();
         rigidBody->setMass(0.0f)
             ->setDrag(1.0f)
             ->setShape(shape)
             ->setStatic(true)
             ->registerToPhysicsManager(PhysicsManager::Instance());
         rigidBody->initialize();
-
-
+    }
+    // Box1
+    {
+        auto Box1 = std::make_shared<GameObject>("Box1");
+        sceneGraph.addNode(Box1);
+        Box1->setLocalPosition(Vector3(-20.0f + offsetX, 1.0f + offsetY, 0.0f + offsetZ));
+        Box1->setLocalScaling(Vector3(10.0f, 5.0f, 20.0f));
+        auto renderComp = Box1->addComponent<Render2D>();
+        renderComp->setCamera(camera)->setMesh(floorMesh)->setMaterial(floorMaterial);
+        auto shape = std::make_shared<OBB>();
+        auto rigidBody = Box1->addComponent<RigidBody>();
+        rigidBody->setMass(0.0f)
+            ->setDrag(1.0f)
+            ->setShape(shape)
+            ->setStatic(true)
+            ->registerToPhysicsManager(PhysicsManager::Instance());
+        rigidBody->initialize();
     }
 
-    //Running Wall
+    // Floor2 pad
+    {
+        auto Floor2 = std::make_shared<GameObject>("Floor2");
+        sceneGraph.addNode(Floor2);
+        Floor2->setLocalPosition(Vector3(-80.0f + offsetX, 0.0f + offsetY, 0.0f + offsetZ));
+        Floor2->setLocalScaling(Vector3(30.0f, 1.0f, 20.0f));
+        auto renderComp = Floor2->addComponent<Render2D>();
+        renderComp->setCamera(camera)->setMesh(floorMesh)->setMaterial(floorMaterial);
+        auto shape = std::make_shared<OBB>();
+        auto rigidBody = Floor2->addComponent<RigidBody>();
+        rigidBody->setMass(0.0f)
+            ->setDrag(1.0f)
+            ->setShape(shape)
+            ->setStatic(true)
+            ->registerToPhysicsManager(PhysicsManager::Instance());
+        rigidBody->initialize();
+    }
+
+    // Pit wall
+    {
+        auto pitWall1 = std::make_shared<GameObject>("pitWall1");
+        sceneGraph.addNode(pitWall1);
+        pitWall1->setLocalPosition(Vector3(-49.5f + offsetX, -5.5f + offsetY, 0.0f + offsetZ));
+        pitWall1->setLocalScaling(Vector3(1.0f, 10.0f, 20.0f));
+        auto renderComp = pitWall1->addComponent<Render2D>();
+        renderComp->setCamera(camera)->setMesh(floorMesh)->setMaterial(floorMaterial);
+        auto shape = std::make_shared<OBB>();
+        auto rigidBody = pitWall1->addComponent<RigidBody>();
+        rigidBody->setMass(0.0f)
+            ->setDrag(1.0f)
+            ->setShape(shape)
+            ->setStatic(true)
+            ->registerToPhysicsManager(PhysicsManager::Instance());
+        rigidBody->initialize();
+    }
+
+    // Pit wall2
+    {
+        auto pitWall2 = std::make_shared<GameObject>("pitWall2");
+        sceneGraph.addNode(pitWall2);
+        pitWall2->setLocalPosition(Vector3(-65.5f + offsetX, -5.5f + offsetY, 0.0f + offsetZ));
+        pitWall2->setLocalScaling(Vector3(1.0f, 10.0f, 20.0f));
+        auto renderComp = pitWall2->addComponent<Render2D>();
+        renderComp->setCamera(camera)->setMesh(floorMesh)->setMaterial(floorMaterial);
+        auto shape = std::make_shared<OBB>();
+        auto rigidBody = pitWall2->addComponent<RigidBody>();
+        rigidBody->setMass(0.0f)
+            ->setDrag(1.0f)
+            ->setShape(shape)
+            ->setStatic(true)
+            ->registerToPhysicsManager(PhysicsManager::Instance());
+        rigidBody->initialize();
+    }
+
+    // Pit floor
+    {
+        auto pitFloor = std::make_shared<GameObject>("pitFloor");
+        sceneGraph.addNode(pitFloor);
+        pitFloor->setLocalPosition(Vector3(-58.0f + offsetX, -10.0f + offsetY, 0.0f + offsetZ));
+        pitFloor->setLocalScaling(Vector3(20.0f, 1.0f, 20.0f));
+        auto renderComp = pitFloor->addComponent<Render2D>();
+        renderComp->setCamera(camera)->setMesh(floorMesh)->setMaterial(floorMaterial);
+        auto shape = std::make_shared<OBB>();
+        auto rigidBody = pitFloor->addComponent<RigidBody>();
+        rigidBody->setMass(0.0f)
+            ->setDrag(1.0f)
+            ->setShape(shape)
+            ->setStatic(true)
+            ->registerToPhysicsManager(PhysicsManager::Instance());
+        rigidBody->initialize();
+    }
+
+    // Pit box
+    {
+        auto pitBox = std::make_shared<GameObject>("pitBox");
+        sceneGraph.addNode(pitBox);
+        pitBox->setLocalPosition(Vector3(-45.0f + offsetX, -10.0f + offsetY, 0.0f + offsetZ));
+        pitBox->setLocalScaling(Vector3(20.0f, 10.0f, 20.0f));
+        auto renderComp = pitBox->addComponent<Render2D>();
+        renderComp->setCamera(camera)->setMesh(floorMesh)->setMaterial(floorMaterial);
+        auto shape = std::make_shared<OBB>();
+        auto rigidBody = pitBox->addComponent<RigidBody>();
+        rigidBody->setMass(0.0f)
+            ->setDrag(1.0f)
+            ->setShape(shape)
+            ->setStatic(true)
+            ->registerToPhysicsManager(PhysicsManager::Instance());
+        rigidBody->initialize();
+    }
+
+    // Floor3 pad
+    {
+        auto Floor3 = std::make_shared<GameObject>("Floor3");
+        sceneGraph.addNode(Floor3);
+        Floor3->setLocalPosition(Vector3(-120.0f + offsetX, 0.0f + offsetY, 0.0f + offsetZ));
+        Floor3->setLocalScaling(Vector3(30.0f, 1.0f, 20.0f));
+        auto renderComp = Floor3->addComponent<Render2D>();
+        renderComp->setCamera(camera)->setMesh(floorMesh)->setMaterial(floorMaterial);
+        auto shape = std::make_shared<OBB>();
+        auto rigidBody = Floor3->addComponent<RigidBody>();
+        rigidBody->setMass(0.0f)
+            ->setDrag(1.0f)
+            ->setShape(shape)
+            ->setStatic(true)
+            ->registerToPhysicsManager(PhysicsManager::Instance());
+        rigidBody->initialize();
+    }
+
+    // Pit wall
+    {
+        auto pitWall1 = std::make_shared<GameObject>("pitWall1");
+        sceneGraph.addNode(pitWall1);
+        pitWall1->setLocalPosition(Vector3(-134.5f + offsetX, -5.5f + offsetY, 0.0f + offsetZ));
+        pitWall1->setLocalScaling(Vector3(1.0f, 10.0f, 20.0f));
+        auto renderComp = pitWall1->addComponent<Render2D>();
+        renderComp->setCamera(camera)->setMesh(floorMesh)->setMaterial(floorMaterial);
+        auto shape = std::make_shared<OBB>();
+        auto rigidBody = pitWall1->addComponent<RigidBody>();
+        rigidBody->setMass(0.0f)
+            ->setDrag(1.0f)
+            ->setShape(shape)
+            ->setStatic(true)
+            ->registerToPhysicsManager(PhysicsManager::Instance());
+        rigidBody->initialize();
+    }
+
+    // Pit wall2
+    {
+        auto pitWall2 = std::make_shared<GameObject>("pitWall2");
+        sceneGraph.addNode(pitWall2);
+        pitWall2->setLocalPosition(Vector3(-184.5f + offsetX, -5.5f + offsetY, 0.0f + offsetZ));
+        pitWall2->setLocalScaling(Vector3(1.0f, 10.0f, 20.0f));
+        auto renderComp = pitWall2->addComponent<Render2D>();
+        renderComp->setCamera(camera)->setMesh(floorMesh)->setMaterial(floorMaterial);
+        auto shape = std::make_shared<OBB>();
+        auto rigidBody = pitWall2->addComponent<RigidBody>();
+        rigidBody->setMass(0.0f)
+            ->setDrag(1.0f)
+            ->setShape(shape)
+            ->setStatic(true)
+            ->registerToPhysicsManager(PhysicsManager::Instance());
+        rigidBody->initialize();
+    }
+
+    // Pit floor 2
+    {
+        auto pitFloor = std::make_shared<GameObject>("pitFloor");
+        sceneGraph.addNode(pitFloor);
+        pitFloor->setLocalPosition(Vector3(-160.0f + offsetX, -10.0f + offsetY, 0.0f + offsetZ));
+        pitFloor->setLocalScaling(Vector3(50.0f, 1.0f, 20.0f));
+        auto renderComp = pitFloor->addComponent<Render2D>();
+        renderComp->setCamera(camera)->setMesh(floorMesh)->setMaterial(floorMaterial);
+        auto shape = std::make_shared<OBB>();
+        auto rigidBody = pitFloor->addComponent<RigidBody>();
+        rigidBody->setMass(0.0f)
+            ->setDrag(1.0f)
+            ->setShape(shape)
+            ->setStatic(true)
+            ->registerToPhysicsManager(PhysicsManager::Instance());
+        rigidBody->initialize();
+    }
+
+    // Pit box2
+    {
+        auto pitBox = std::make_shared<GameObject>("pitBox");
+        sceneGraph.addNode(pitBox);
+        pitBox->setLocalPosition(Vector3(-135.0f + offsetX, -10.0f + offsetY, 0.0f + offsetZ));
+        pitBox->setLocalScaling(Vector3(10.0f, 10.0f, 20.0f));
+        auto renderComp = pitBox->addComponent<Render2D>();
+        renderComp->setCamera(camera)->setMesh(floorMesh)->setMaterial(floorMaterial);
+        auto shape = std::make_shared<OBB>();
+        auto rigidBody = pitBox->addComponent<RigidBody>();
+        rigidBody->setMass(0.0f)
+            ->setDrag(1.0f)
+            ->setShape(shape)
+            ->setStatic(true)
+            ->registerToPhysicsManager(PhysicsManager::Instance());
+        rigidBody->initialize();
+    }
+
+    // Wall Running
     {
         auto wallRunWall = std::make_shared<GameObject>("WallRunWall", GameObject::RUNNABLE_WALL);
         sceneGraph.addNode(wallRunWall);
-        wallRunWall->setLocalPosition(Vector3(0.0f + offsetX, 2.5f + offsetY, -31.0f + offsetZ));
+        wallRunWall->setLocalPosition(Vector3(-160.0f + offsetX, 4.0f + offsetY, -10.0f + offsetZ));
         wallRunWall->setLocalScaling(Vector3(60.0f, 12.0f, 1.0f));
         auto renderComp = wallRunWall->addComponent<Render2D>();
         renderComp->setCamera(camera)->setMesh(floorMesh)->setMaterial(floorMaterial);
@@ -103,6 +333,113 @@ void MapLoader::zero(float offsetX, float offsetY, float offsetZ, SceneGraph& sc
             ->registerToPhysicsManager(PhysicsManager::Instance());
         rigidBody->initialize();
     }
+
+    // Floor4 pad
+    {
+        auto Floor4 = std::make_shared<GameObject>("Floor4");
+        sceneGraph.addNode(Floor4);
+        Floor4->setLocalPosition(Vector3(-199.0f + offsetX, 0.0f + offsetY, 0.0f + offsetZ));
+        Floor4->setLocalScaling(Vector3(30.0f, 1.0f, 20.0f));
+        auto renderComp = Floor4->addComponent<Render2D>();
+        renderComp->setCamera(camera)->setMesh(floorMesh)->setMaterial(floorMaterial);
+        auto shape = std::make_shared<OBB>();
+        auto rigidBody = Floor4->addComponent<RigidBody>();
+        rigidBody->setMass(0.0f)
+            ->setDrag(1.0f)
+            ->setShape(shape)
+            ->setStatic(true)
+            ->registerToPhysicsManager(PhysicsManager::Instance());
+        rigidBody->initialize();
+    }
+
+    // Wall Running 2
+    {
+        auto wallRunWall = std::make_shared<GameObject>("WallRunWall", GameObject::RUNNABLE_WALL);
+        sceneGraph.addNode(wallRunWall);
+        wallRunWall->setLocalPosition(Vector3(-230.0f + offsetX, 4.0f + offsetY, 10.0f + offsetZ));
+        wallRunWall->setLocalScaling(Vector3(60.0f, 12.0f, 1.0f));
+        auto renderComp = wallRunWall->addComponent<Render2D>();
+        renderComp->setCamera(camera)->setMesh(floorMesh)->setMaterial(floorMaterial);
+        auto shape = std::make_shared<OBB>();
+        auto rigidBody = wallRunWall->addComponent<RigidBody>();
+        rigidBody->setMass(0.0f)
+            ->setDrag(1.0f)
+            ->setShape(shape)
+            ->setStatic(true)
+            ->registerToPhysicsManager(PhysicsManager::Instance());
+        rigidBody->initialize();
+    }
+
+    // Floor5 pad
+    {
+        auto Floor5 = std::make_shared<GameObject>("Floor5");
+        sceneGraph.addNode(Floor5);
+        Floor5->setLocalPosition(Vector3(-259.0f + offsetX, 0.0f + offsetY, 0.0f + offsetZ));
+        Floor5->setLocalScaling(Vector3(30.0f, 1.0f, 20.0f));
+        auto renderComp = Floor5->addComponent<Render2D>();
+        renderComp->setCamera(camera)->setMesh(floorMesh)->setMaterial(floorMaterial);
+        auto shape = std::make_shared<OBB>();
+        auto rigidBody = Floor5->addComponent<RigidBody>();
+        rigidBody->setMass(0.0f)
+            ->setDrag(1.0f)
+            ->setShape(shape)
+            ->setStatic(true)
+            ->registerToPhysicsManager(PhysicsManager::Instance());
+        rigidBody->initialize();
+    }
+
+    // Wall Running 3 & 4
+        {
+        auto wallRunWall = std::make_shared<GameObject>("WallRunWall", GameObject::RUNNABLE_WALL);
+        sceneGraph.addNode(wallRunWall);
+        wallRunWall->setLocalPosition(Vector3(-295.0f + offsetX, 4.0f + offsetY, -10.0f + offsetZ));
+        wallRunWall->setLocalScaling(Vector3(60.0f, 12.0f, 1.0f));
+        auto renderComp = wallRunWall->addComponent<Render2D>();
+        renderComp->setCamera(camera)->setMesh(floorMesh)->setMaterial(floorMaterial);
+        auto shape = std::make_shared<OBB>();
+        auto rigidBody = wallRunWall->addComponent<RigidBody>();
+        rigidBody->setMass(0.0f)
+            ->setDrag(1.0f)
+            ->setShape(shape)
+            ->setStatic(true)
+            ->registerToPhysicsManager(PhysicsManager::Instance());
+        rigidBody->initialize();
+    }
+    {
+        auto wallRunWall = std::make_shared<GameObject>("WallRunWall", GameObject::RUNNABLE_WALL);
+        sceneGraph.addNode(wallRunWall);
+        wallRunWall->setLocalPosition(Vector3(-350.0f + offsetX, 4.0f + offsetY, 10.0f + offsetZ));
+        wallRunWall->setLocalScaling(Vector3(60.0f, 12.0f, 1.0f));
+        auto renderComp = wallRunWall->addComponent<Render2D>();
+        renderComp->setCamera(camera)->setMesh(floorMesh)->setMaterial(floorMaterial);
+        auto shape = std::make_shared<OBB>();
+        auto rigidBody = wallRunWall->addComponent<RigidBody>();
+        rigidBody->setMass(0.0f)
+            ->setDrag(1.0f)
+            ->setShape(shape)
+            ->setStatic(true)
+            ->registerToPhysicsManager(PhysicsManager::Instance());
+        rigidBody->initialize();
+    }
+
+    // Floor6 pad
+    {
+        auto Floor6 = std::make_shared<GameObject>("Floor6");
+        sceneGraph.addNode(Floor6);
+        Floor6->setLocalPosition(Vector3(-390.0f + offsetX, 0.0f + offsetY, 0.0f + offsetZ));
+        Floor6->setLocalScaling(Vector3(30.0f, 1.0f, 20.0f));
+        auto renderComp = Floor6->addComponent<Render2D>();
+        renderComp->setCamera(camera)->setMesh(floorMesh)->setMaterial(floorMaterial);
+        auto shape = std::make_shared<OBB>();
+        auto rigidBody = Floor6->addComponent<RigidBody>();
+        rigidBody->setMass(0.0f)
+            ->setDrag(1.0f)
+            ->setShape(shape)
+            ->setStatic(true)
+            ->registerToPhysicsManager(PhysicsManager::Instance());
+        rigidBody->initialize();
+    }
+    
 
 }
 
