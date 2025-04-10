9047ddb79f6e00d7bc1539e5f161632697f53230
Anish Murthy <anish.murthy@digipen.edu>
2025-04-09 22:08:03 -0700

    FBO class completed

--- AnishCS529Engine/AnishCS529Engine/src/FBO.cpp
+++ AnishCS529Engine/AnishCS529Engine/src/FBO.cpp
@@ -1,53 +1,55 @@
+/*!****************************************************************************
+ * \file   FBO.cpp
+ * \author Anish Murthy (anish.murthy.dev@gmail.com)
+ * \par    **DigiPen Email**
+ *    anish.murthy@digipen.edu
+ * \date   04-09-2025
+ * 
+ *****************************************************************************/
 #include "precompiled.h"
 #include "FBO.h"
 
-#include <glbinding/gl/gl.h>
-#include <glbinding/Binding.h>
-using namespace gl;
-
-#include "fbo.h"
-
-void FBO::createFBO(const int w, const int h) {
-  width = w;
-  height = h;
-
-  glGenFramebuffersEXT(1, &fboID);
-  glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fboID);
+void FBO::initialize() {
+  glGenFramebuffers(1, &fboID);
+  glBindFramebuffer(GL_FRAMEBUFFER, fboID);
 
   // Create a render buffer, and attach it to FBO's depth attachment
   unsigned int depthBuffer;
-  glGenRenderbuffersEXT(1, &depthBuffer);
-  glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, depthBuffer);
-  glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, GL_DEPTH_COMPONENT,
+  glGenRenderbuffers(1, &depthBuffer);
+  glBindRenderbuffer(GL_RENDERBUFFER, depthBuffer);
+  glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT,
     width, height);
-  glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT, GL_DEPTH_ATTACHMENT_EXT,
-    GL_RENDERBUFFER_EXT, depthBuffer);
+  glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT,
+    GL_RENDERBUFFER, depthBuffer);
 
   // Check for completeness/correctness
-  int status = (int)glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT);
-  if ( status != int(GL_FRAMEBUFFER_COMPLETE_EXT) )
+  int status = (int)glCheckFramebufferStatus(GL_FRAMEBUFFER);
+  if ( status != int(GL_FRAMEBUFFER_COMPLETE) )
     printf("FBO Error: %d\n", status);
 
   // Unbind the fbo until it's ready to be used
-  glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
-void FBO::setViewport(const int w, const int h) {}
+void FBO::setViewport(const int w, const int h) {
+  width = w;
+  height = h;
+}
 
 void FBO::attachTexture(TextureManager::TextureID textureID) {
 
-  glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fboID);
+  glBindFramebuffer(GL_FRAMEBUFFER, fboID);
 
-  glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT,
+  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
     GL_TEXTURE_2D, textureID, 0);
 
   // Check for completeness/correctness
-  int status = (int)glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT);
-  if ( status != int(GL_FRAMEBUFFER_COMPLETE_EXT) )
+  int status = (int)glCheckFramebufferStatus(GL_FRAMEBUFFER);
+  if ( status != int(GL_FRAMEBUFFER_COMPLETE) )
     printf("FBO Error: %d\n", status);
 
-  glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
-void FBO::bindFBO() { glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fboID); }
-void FBO::unbindFBO() { glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0); }
+void FBO::bindFBO() { glBindFramebuffer(GL_FRAMEBUFFER, fboID); }
+void FBO::unbindFBO() { glBindFramebuffer(GL_FRAMEBUFFER, 0); }
