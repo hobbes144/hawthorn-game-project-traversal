2c9efe33be0d10eea573b9a86bce5d90c4c7e49c
lixiaok <lixiaok@uw.edu>
2025-04-17 09:57:22 -0700

    Keybinding

--- AnishCS529Engine/AnishCS529Engine/src/PauseMenu.cpp
+++ AnishCS529Engine/AnishCS529Engine/src/PauseMenu.cpp
@@ -233,269 +233,300 @@ void PauseMenu::howToPlay() {
  * 
  *****************************************************************************/
 void PauseMenu::settings() {
-	ImGuiIO& io = ImGui::GetIO();
-	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable keyboard controls
-	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad; // Enable gamepad controls
-	io.FontGlobalScale = 2.0f;
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+    io.FontGlobalScale = 2.0f;
 
-	if (ImGui::IsKeyDown(KEY_ENTER)) {
-		io.KeyMap[ImGuiKey_Space] = KEY_ENTER;
-	}
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
 
-	if (ImGui::IsKeyDown(KEY_SPACE)) {
-		io.KeyMap[ImGuiKey_Space] = KEY_SPACE;
-	}
+    ImVec2 displaySize = ImGui::GetIO().DisplaySize;
+    ImGui::SetNextWindowPos(ImVec2(0, 0));
+    ImGui::SetNextWindowSize(displaySize);
 
-	ImGui_ImplOpenGL3_NewFrame();
-	ImGui_ImplGlfw_NewFrame();
-	ImGui::NewFrame();
+    ImGui::Begin("Settings", nullptr,
+        ImGuiWindowFlags_NoDecoration |
+        ImGuiWindowFlags_NoMove |
+        ImGuiWindowFlags_NoResize |
+        ImGuiWindowFlags_NoSavedSettings |
+        ImGuiWindowFlags_NoBringToFrontOnFocus);
 
-	// Get the display size
-	ImVec2 displaySize = ImGui::GetIO().DisplaySize;
+    ImDrawList* drawList = ImGui::GetBackgroundDrawList();
+    drawList->AddRectFilled(ImVec2(0, 0), displaySize, IM_COL32(0, 0, 0, 96));
 
-	// Set next window to cover the entire screen
-	ImGui::SetNextWindowPos(ImVec2(0, 0));
-	ImGui::SetNextWindowSize(displaySize);
+    ImVec2 center = ImVec2(displaySize.x / 2, displaySize.y / 2);
+    ImGui::SetCursorPos(ImVec2(center.x - 70, center.y - 300));
 
-	ImGui::Begin("Settings", nullptr,
-		ImGuiWindowFlags_NoDecoration |
-		ImGuiWindowFlags_NoMove |
-		ImGuiWindowFlags_NoResize |
-		ImGuiWindowFlags_NoSavedSettings |
-		ImGuiWindowFlags_NoBringToFrontOnFocus);
+    float windowWidth = ImGui::GetWindowSize().x;
+    ImGui::Text("Settings");
+    ImGui::Text(" ");
 
-	ImDrawList* drawList = ImGui::GetBackgroundDrawList();
-	drawList->AddRectFilled(ImVec2(0, 0), displaySize, IM_COL32(0, 0, 0, 96));
+    float buttonWidth = 130.0f;
+    float itemSpacing = ImGui::GetStyle().ItemSpacing.x;
 
-	ImVec2 center = ImVec2(displaySize.x / 2, displaySize.y / 2);
-	ImGui::SetCursorPos(ImVec2(center.x - 70, center.y - 300));
+    ImGui::PushItemWidth(400.0f);
 
-	float windowWidth = ImGui::GetWindowSize().x;
-	float textWidth = ImGui::CalcTextSize("Settings").x;
-	ImGui::Text("Settings");
+    //---------------- VOLUME SLIDERS -----------------
+    ImGui::SetCursorPosX((windowWidth - 400.f) * 0.5f);
 
-	ImGui::Text(" ");
+    ImGui::SetCursorPosX((windowWidth - 400.f) * 0.5f);
+    ImGui::SliderFloat("Music Volume", &musicVolume, 0.0f, 1.0f);
+    ImGui::SetCursorPosX((windowWidth - 400.f) * 0.5f);
+    ImGui::SliderFloat("SFX Volume", &SFXVolume, 0.0f, 1.0f);
 
-	float buttonWidth = 200.0f;
+    ImGui::SetCursorPosX((windowWidth - 400.f) * 0.5f);
+    ImGui::SliderFloat("MouseX Sensivity", &mouseXSensivity, 0.0f, 0.2f);
+    ImGui::SetCursorPosX((windowWidth - 400.f) * 0.5f);
+    ImGui::SliderFloat("MouseY Sensivity", &mouseYSensivity, 0.0f, 0.2f);
 
-	ImGui::PushItemWidth(400.0f);
+    ImGui::Text(" ");
 
-#pragma region Volume
+    // ------------ KEY BINDINGS ----------------
+    enum BindAction {
+        MoveForward,
+        MoveBackward,
+        MoveLeft,
+        MoveRight,
+        Sprint,
+        Slide,
+        ACTION_COUNT
+    };
+    static const char* actionLabels[ACTION_COUNT] = {
+        "Move Forward", "Move Backward", "Move Left", "Move Right", "Sprint", "Slide"
+    };
+    static Key actionKeys[ACTION_COUNT] = {
+        KEY_W, KEY_S, KEY_A, KEY_D, KEY_LEFT_SHIFT, KEY_LEFT_CONTROL
+    };
+    static int keyToChange = -1;
+    static bool awaitingKey = false;
 
-	ImGui::SetCursorPosX((windowWidth - buttonWidth) * 0.5f - 100.0f);
-	ImGui::SliderFloat("Music Volume", &musicVolume, 0.0f, 1.0f);
+    // Key assignment
+    if (awaitingKey && keyToChange >= 0 && keyToChange < ACTION_COUNT) {
+        for (int k = 32; k <= 348; ++k) // GLFW keys 32-348
+        {
+            if (input->isKeyPressed((Key)k)) {
+                // ESCAPE used as cancel only
+                if (k == GLFW_KEY_ESCAPE) {
+                    keyToChange = -1;
+                    awaitingKey = false;
+                }
+                else {
+                    actionKeys[keyToChange] = (Key)k;
+                    keyToChange = -1;
+                    awaitingKey = false;
+                }
+                break;
+            }
+        }
+    }
 
-	if ((ImGui::IsKeyPressed(KEY_LEFT) && ImGui::IsItemFocused()) ||
-		(gp->isPressed(XINPUT_GAMEPAD_DPAD_LEFT) && ImGui::IsItemFocused())) {
-		if (musicVolume > 0.01f) {
-			musicVolume -= 0.05f;
-		}
-		else {
-			musicVolume = 0.0f;
-		}
-	}
-	if ((ImGui::IsKeyPressed(KEY_RIGHT) && ImGui::IsItemFocused()) ||
-		(gp->isPressed(XINPUT_GAMEPAD_DPAD_RIGHT) && ImGui::IsItemFocused())) {
-		if (musicVolume < 0.99f) {
-			musicVolume += 0.05f;
-		}
-		else {
-			musicVolume = 1.0f;
-		}
-	}
+    // Key Label Mapping
+    auto GetKeyLabel = [](Key k)->std::string {
+        switch (k) {
+        case KEY_SPACE: return "Space";
+        case GLFW_KEY_APOSTROPHE: return "'";
+        case GLFW_KEY_COMMA: return ",";
+        case GLFW_KEY_MINUS: return "-";
+        case GLFW_KEY_PERIOD: return ".";
+        case GLFW_KEY_SLASH: return "/";
+        case KEY_0: return "0";
+        case KEY_1: return "1";
+        case KEY_2: return "2";
+        case KEY_3: return "3";
+        case KEY_4: return "4";
+        case KEY_5: return "5";
+        case KEY_6: return "6";
+        case KEY_7: return "7";
+        case KEY_8: return "8";
+        case KEY_9: return "9";
+        case GLFW_KEY_SEMICOLON: return ";";
+        case GLFW_KEY_EQUAL: return "=";
+        case KEY_A: return "A";
+        case KEY_B: return "B";
+        case KEY_C: return "C";
+        case KEY_D: return "D";
+        case KEY_E: return "E";
+        case KEY_F: return "F";
+        case KEY_G: return "G";
+        case KEY_H: return "H";
+        case KEY_I: return "I";
+        case KEY_J: return "J";
+        case KEY_K: return "K";
+        case KEY_L: return "L";
+        case KEY_M: return "M";
+        case KEY_N: return "N";
+        case KEY_O: return "O";
+        case KEY_P: return "P";
+        case KEY_Q: return "Q";
+        case KEY_R: return "R";
+        case KEY_S: return "S";
+        case KEY_T: return "T";
+        case KEY_U: return "U";
+        case KEY_V: return "V";
+        case KEY_W: return "W";
+        case KEY_X: return "X";
+        case KEY_Y: return "Y";
+        case KEY_Z: return "Z";
+        case GLFW_KEY_LEFT_BRACKET: return "[";
+        case GLFW_KEY_BACKSLASH: return "\\";
+        case GLFW_KEY_RIGHT_BRACKET: return "]";
+        case GLFW_KEY_GRAVE_ACCENT: return "`";
+        case GLFW_KEY_WORLD_1:      return "WLD1";
+        case GLFW_KEY_WORLD_2:      return "WLD2";
+        // Control/Function
+        case GLFW_KEY_ESCAPE:       return "Esc";
+        case GLFW_KEY_ENTER:        return "Enter";
+        case GLFW_KEY_TAB:          return "Tab";
+        case GLFW_KEY_BACKSPACE:    return "Backspace";
+        case GLFW_KEY_INSERT:       return "Insert";
+        case GLFW_KEY_DELETE:       return "Delete";
+        case GLFW_KEY_RIGHT:        return "Right";
+        case GLFW_KEY_LEFT:         return "Left";
+        case GLFW_KEY_DOWN:         return "Down";
+        case GLFW_KEY_UP:           return "Up";
+        case GLFW_KEY_PAGE_UP:      return "PageUp";
+        case GLFW_KEY_PAGE_DOWN:    return "PageDown";
+        case GLFW_KEY_HOME:         return "Home";
+        case GLFW_KEY_END:          return "End";
+        case GLFW_KEY_CAPS_LOCK:    return "Caps";
+        case GLFW_KEY_SCROLL_LOCK:  return "Scroll";
+        case GLFW_KEY_NUM_LOCK:     return "NumLock";
+        case GLFW_KEY_PRINT_SCREEN: return "PrtScrn";
+        case GLFW_KEY_PAUSE:        return "Pause";
+        // Function keys
+        case GLFW_KEY_F1:  return "F1";
+        case GLFW_KEY_F2:  return "F2";
+        case GLFW_KEY_F3:  return "F3";
+        case GLFW_KEY_F4:  return "F4";
+        case GLFW_KEY_F5:  return "F5";
+        case GLFW_KEY_F6:  return "F6";
+        case GLFW_KEY_F7:  return "F7";
+        case GLFW_KEY_F8:  return "F8";
+        case GLFW_KEY_F9:  return "F9";
+        case GLFW_KEY_F10: return "F10";
+        case GLFW_KEY_F11: return "F11";
+        case GLFW_KEY_F12: return "F12";
+        case GLFW_KEY_F13: return "F13";
+        case GLFW_KEY_F14: return "F14";
+        case GLFW_KEY_F15: return "F15";
+        case GLFW_KEY_F16: return "F16";
+        case GLFW_KEY_F17: return "F17";
+        case GLFW_KEY_F18: return "F18";
+        case GLFW_KEY_F19: return "F19";
+        case GLFW_KEY_F20: return "F20";
+        case GLFW_KEY_F21: return "F21";
+        case GLFW_KEY_F22: return "F22";
+        case GLFW_KEY_F23: return "F23";
+        case GLFW_KEY_F24: return "F24";
+        case GLFW_KEY_F25: return "F25";
+        // Numpad
+        case GLFW_KEY_KP_0: return "Num 0";
+        case GLFW_KEY_KP_1: return "Num 1";
+        case GLFW_KEY_KP_2: return "Num 2";
+        case GLFW_KEY_KP_3: return "Num 3";
+        case GLFW_KEY_KP_4: return "Num 4";
+        case GLFW_KEY_KP_5: return "Num 5";
+        case GLFW_KEY_KP_6: return "Num 6";
+        case GLFW_KEY_KP_7: return "Num 7";
+        case GLFW_KEY_KP_8: return "Num 8";
+        case GLFW_KEY_KP_9: return "Num 9";
+        case GLFW_KEY_KP_DECIMAL:  return "Num .";
+        case GLFW_KEY_KP_DIVIDE:   return "Num /";
+        case GLFW_KEY_KP_MULTIPLY: return "Num *";
+        case GLFW_KEY_KP_SUBTRACT: return "Num -";
+        case GLFW_KEY_KP_ADD:      return "Num +";
+        case GLFW_KEY_KP_ENTER:    return "Num Enter";
+        case GLFW_KEY_KP_EQUAL:    return "Num =";
+        // Modifiers
+        case GLFW_KEY_LEFT_SHIFT:   return "Shift";
+        case GLFW_KEY_LEFT_CONTROL: return "Ctrl";
+        case GLFW_KEY_LEFT_ALT:     return "Alt";
+        case GLFW_KEY_LEFT_SUPER:   return "Super";
+        case GLFW_KEY_RIGHT_SHIFT:   return "RShift";
+        case GLFW_KEY_RIGHT_CONTROL: return "RCtrl";
+        case GLFW_KEY_RIGHT_ALT:     return "RAlt";
+        case GLFW_KEY_RIGHT_SUPER:   return "RSuper";
+        case GLFW_KEY_MENU:         return "Menu";
+        default: return std::string("Key ") + std::to_string((int)k);
+        }
+    };
 
-	ImGui::SetCursorPosX((windowWidth - buttonWidth) * 0.5f - 100.0f);
-	ImGui::SliderFloat("SFX Volume", &SFXVolume, 0.0f, 1.0f);
+    //----------------------------------------------
+    // CENTERED TWO COLUMN KEYBIND UI
+    //----------------------------------------------
+        std::string title = "Key Bindings";
+        ImVec2 titleSize = ImGui::CalcTextSize(title.c_str());
+        ImGui::SetCursorPosX((windowWidth - titleSize.x) * 0.5f);
+        ImGui::Text("%s", title.c_str());
 
-	if ((ImGui::IsKeyPressed(KEY_LEFT) && ImGui::IsItemFocused()) || 
-		(gp->isPressed(XINPUT_GAMEPAD_DPAD_LEFT) && ImGui::IsItemFocused())) {
-		if (SFXVolume > 0.01f) {
-			SFXVolume -= 0.05f;
-		}
-		else {
-			SFXVolume = 0.0f;
-		}
-	}
-	if ((ImGui::IsKeyPressed(KEY_RIGHT) && ImGui::IsItemFocused()) ||
-		(gp->isPressed(XINPUT_GAMEPAD_DPAD_RIGHT) && ImGui::IsItemFocused())) {
-		if (SFXVolume < 0.99f) {
-			SFXVolume += 0.05f;
-		}
-		else {
-			SFXVolume = 1.0f;
-		}
-	}
+        // --- Centered binding rows ---
+        float totalBindWidth = 400.f;
+        float margin = (windowWidth - totalBindWidth) * 0.5f;
+        float labelWidth = 200.f;
+        float cellHeight = ImGui::GetTextLineHeightWithSpacing();
 
-#pragma endregion
+        for (int i = 0; i < ACTION_COUNT; ++i)
+        {
+            float row_start_y = ImGui::GetCursorPosY();
+            ImGui::SetCursorPosX(margin);
+            ImGui::AlignTextToFramePadding();
+            ImGui::Text("%s", actionLabels[i]);
+            ImGui::SameLine(margin + labelWidth);
 
-	ImGui::Text(" ");
+            std::string buttonId = std::string("##keybind") + std::to_string(i);
+            std::string btnLabel = (keyToChange == i && awaitingKey) ? "Press key..." : GetKeyLabel(actionKeys[i]);
 
-#pragma region Mouse
+            if (ImGui::Button((btnLabel + buttonId).c_str(), ImVec2(buttonWidth, 0)))
+            {
+                keyToChange = i;
+                awaitingKey = true;
+            }
+            ImGui::SetCursorPosY(row_start_y + cellHeight);
+        }
 
-	ImGui::SetCursorPosX((windowWidth - buttonWidth) * 0.5f - 100.0f);
-	ImGui::SliderFloat("MouseX Sensivity", &mouseXSensivity, 0.0f, 0.2f);
+    ImGui::Text(" ");
+    ImGui::PopItemWidth();
 
-	if ((ImGui::IsKeyPressed(KEY_LEFT) && ImGui::IsItemFocused()) ||
-		(gp->isPressed(XINPUT_GAMEPAD_DPAD_LEFT) && ImGui::IsItemFocused())) {
-		if (mouseXSensivity > 0.005f) {
-			mouseXSensivity -= 0.01f;
-		}
-		else {
-			mouseXSensivity = 0.0f;
-		}
-	}
-	if ((ImGui::IsKeyPressed(KEY_RIGHT) && ImGui::IsItemFocused()) ||
-		(gp->isPressed(XINPUT_GAMEPAD_DPAD_RIGHT) && ImGui::IsItemFocused())) {
-		if (mouseXSensivity < 0.195f) {
-			mouseXSensivity += 0.01f;
-		}
-		else {
-			mouseXSensivity = 0.2f;
-		}
-	}
+    // Apply key binding
+    if (player) {
+        auto fpc = player->findComponent<FirstPersonControllerComponent>();
+        if (fpc) {
+            fpc->setActionKey(FirstPersonControllerComponent::MoveForward, actionKeys[MoveForward]);
+            fpc->setActionKey(FirstPersonControllerComponent::MoveBackward, actionKeys[MoveBackward]);
+            fpc->setActionKey(FirstPersonControllerComponent::MoveLeft, actionKeys[MoveLeft]);
+            fpc->setActionKey(FirstPersonControllerComponent::MoveRight, actionKeys[MoveRight]);
+            fpc->setActionKey(FirstPersonControllerComponent::Sprint, actionKeys[Sprint]);
+            fpc->setActionKey(FirstPersonControllerComponent::Slide, actionKeys[Slide]);
+        }
+    }
 
-	ImGui::SetCursorPosX((windowWidth - buttonWidth) * 0.5f - 100.0f);
-	ImGui::SliderFloat("MouseY Sensivity", &mouseYSensivity, 0.0f, 0.2f);
+    AudioManager::instance().setVolume("music", musicVolume);
 
-	if ((ImGui::IsKeyPressed(KEY_LEFT) && ImGui::IsItemFocused()) ||
-		(gp->isPressed(XINPUT_GAMEPAD_DPAD_LEFT) && ImGui::IsItemFocused())) {
-		if (mouseYSensivity > 0.005f) {
-			mouseYSensivity -= 0.01f;
-		}
-		else {
-			mouseYSensivity = 0.0f;
-		}
-	}
-	if ((ImGui::IsKeyPressed(KEY_RIGHT) && ImGui::IsItemFocused()) ||
-		(gp->isPressed(XINPUT_GAMEPAD_DPAD_RIGHT) && ImGui::IsItemFocused())) {
-		if (mouseYSensivity < 0.195f) {
-			mouseYSensivity += 0.01f;
-		}
-		else {
-			mouseYSensivity = 0.2f;
-		}
-	}
+    if (player) {
+        auto fpc = player->findComponent<FirstPersonControllerComponent>();
+        if (fpc) {
+            fpc->setMouseXSensivity(mouseXSensivity);
+            fpc->setMouseYSensivity(mouseYSensivity);
+        }
+    }
+    if (gp) {
+        gp->setLXSensivity(gpLXSensitivity);
+        gp->setLYSensivity(gpLYSensitivity);
+        gp->setRXSensivity(gpRXSensitivity);
+        gp->setRYSensivity(gpRYSensitivity);
+    }
 
-#pragma endregion
+    ImGui::SetCursorPosX((windowWidth - buttonWidth) * 0.5f);
+    if ((ImGui::Button("Go Back", ImVec2(buttonWidth, 40)) ||
+        (ImGui::IsKeyPressed(KEY_ESCAPE) && !awaitingKey))) {
+        menuType = MainPauseMenu;
+    }
 
-	ImGui::Text(" ");
-
-#pragma region GamePad
-
-	ImGui::SetCursorPosX((windowWidth - buttonWidth) * 0.5f - 100.0f);
-	ImGui::SliderFloat("Left Joystick X Sensivity", &gpLXSensitivity, 0.0f, 6.0f);
-
-	if ((ImGui::IsKeyPressed(KEY_LEFT) && ImGui::IsItemFocused()) ||
-		(gp->isPressed(XINPUT_GAMEPAD_DPAD_LEFT) && ImGui::IsItemFocused())) {
-		if (gpLXSensitivity > 0.05f) {
-			gpLXSensitivity -= 0.25f;
-		}
-		else {
-			gpLXSensitivity = 0.0f;
-		}
-	}
-	if ((ImGui::IsKeyPressed(KEY_RIGHT) && ImGui::IsItemFocused()) ||
-		(gp->isPressed(XINPUT_GAMEPAD_DPAD_RIGHT) && ImGui::IsItemFocused())) {
-		if (gpLXSensitivity < 5.95f) {
-			gpLXSensitivity += 0.25f;
-		}
-		else {
-			gpLXSensitivity = 6.0f;
-		}
-	}
-
-	ImGui::SetCursorPosX((windowWidth - buttonWidth) * 0.5f - 100.0f);
-	ImGui::SliderFloat("Left Joystick Y Sensivity", &gpLYSensitivity, 0.0f, 6.0f);
-
-	if ((ImGui::IsKeyPressed(KEY_LEFT) && ImGui::IsItemFocused()) ||
-		(gp->isPressed(XINPUT_GAMEPAD_DPAD_LEFT) && ImGui::IsItemFocused())) {
-		if (gpLYSensitivity > 0.05f) {
-			gpLYSensitivity -= 0.25f;
-		}
-		else {
-			gpLYSensitivity = 0.0f;
-		}
-	}
-	if ((ImGui::IsKeyPressed(KEY_RIGHT) && ImGui::IsItemFocused()) ||
-		(gp->isPressed(XINPUT_GAMEPAD_DPAD_RIGHT) && ImGui::IsItemFocused())) {
-		if (gpLYSensitivity < 5.95f) {
-			gpLYSensitivity += 0.25f;
-		}
-		else {
-			gpLYSensitivity = 6.0f;
-		}
-	}
-
-	ImGui::SetCursorPosX((windowWidth - buttonWidth) * 0.5f - 100.0f);
-	ImGui::SliderFloat("Right Joystick X Sensivity", &gpRXSensitivity, 0.0f, 6.0f);
-
-	if ((ImGui::IsKeyPressed(KEY_LEFT) && ImGui::IsItemFocused()) ||
-		(gp->isPressed(XINPUT_GAMEPAD_DPAD_LEFT) && ImGui::IsItemFocused())) {
-		if (gpRXSensitivity > 0.05f) {
-			gpRXSensitivity -= 0.25f;
-		}
-		else {
-			gpRXSensitivity = 0.0f;
-		}
-	}
-	if ((ImGui::IsKeyPressed(KEY_RIGHT) && ImGui::IsItemFocused()) ||
-		(gp->isPressed(XINPUT_GAMEPAD_DPAD_RIGHT) && ImGui::IsItemFocused())) {
-		if (gpRXSensitivity < 5.95f) {
-			gpRXSensitivity += 0.25f;
-		}
-		else {
-			gpRXSensitivity = 6.0f;
-		}
-	}
-
-	ImGui::SetCursorPosX((windowWidth - buttonWidth) * 0.5f - 100.0f);
-	ImGui::SliderFloat("Right Joystick Y Sensivity", &gpRYSensitivity, 0.0f, 6.0f);
-
-	if ((ImGui::IsKeyPressed(KEY_LEFT) && ImGui::IsItemFocused()) ||
-		(gp->isPressed(XINPUT_GAMEPAD_DPAD_LEFT) && ImGui::IsItemFocused())) {
-		if (gpRYSensitivity > 0.05f) {
-			gpRYSensitivity -= 0.25f;
-		}
-		else {
-			gpRYSensitivity = 0.0f;
-		}
-	}
-	if ((ImGui::IsKeyPressed(KEY_RIGHT) && ImGui::IsItemFocused()) ||
-		(gp->isPressed(XINPUT_GAMEPAD_DPAD_RIGHT) && ImGui::IsItemFocused())) {
-		if (gpRYSensitivity < 5.95f) {
-			gpRYSensitivity += 0.25f;
-		}
-		else {
-			gpRYSensitivity = 6.0f;
-		}
-	}
-
-#pragma endregion
-
-	ImGui::Text(" ");
-
-	ImGui::PopItemWidth();
-
-	AudioManager::instance().setVolume("music", musicVolume);
-	player->findComponent<FirstPersonControllerComponent>()->setMouseXSensivity(mouseXSensivity);
-	player->findComponent<FirstPersonControllerComponent>()->setMouseYSensivity(mouseYSensivity);
-	gp->setLXSensivity(gpLXSensitivity);
-	gp->setLYSensivity(gpLYSensitivity);
-	gp->setRXSensivity(gpRXSensitivity);
-	gp->setRYSensivity(gpRYSensitivity);
-
-	ImGui::SetCursorPosX((windowWidth - buttonWidth) * 0.5f);
-	if (ImGui::Button("Go Back", ImVec2(buttonWidth, 40)) ||
-		(ImGui::IsKeyPressed(KEY_ESCAPE))) {
-		menuType = MainPauseMenu;
-	}
-
-	ImGui::End();
-
-	ImGui::Render();
-	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-	return;
+    ImGui::End();
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 
 void PauseMenu::quitMenu() {
--- AnishCS529Engine/AnishCS529Engine/src/PauseMenu.h
+++ AnishCS529Engine/AnishCS529Engine/src/PauseMenu.h
@@ -23,6 +23,7 @@
 #include "imgui_impl_opengl3.h"
 #include "Input.h"
 #include "stb/stb_image.h"
+#include <variant>
 
 class PauseMenu {
 public:
@@ -57,6 +58,30 @@ private:
 		Test
 	};
 
+	enum Action {
+		MoveForward,
+		MoveBackward,
+		MoveLeft,
+		MoveRight,
+		Sprint,
+		Slide,
+		NUM_ACTIONS
+	};
+	const char* ActionNames[NUM_ACTIONS] = {
+		"Move Forward", "Move Backward", "Move Left", "Move Right", "Sprint", "Slide"
+	};
+
+	using BindInput = std::variant<Key, int>; 
+
+	// For each action, store the bind
+	BindInput actionBinds[NUM_ACTIONS] = {
+		KEY_W, KEY_S, KEY_A, KEY_D, KEY_LEFT_SHIFT, KEY_LEFT_CONTROL
+	};
+
+	// For remapping state
+	int remapActionIndex = -1; // -1:not remapping, [0,NUM_ACTIONS)
+	bool waitingForKey = false;
+
 	PauseMenu() = default;
 
 	void mainPauseMenu();
@@ -85,6 +110,31 @@ private:
 	FFramerateController* frc;
 	GamePad* gp;
 	std::shared_ptr<GameObject> player;
+
+	static std::string GetBindName(const BindInput& bind)
+	{
+		if (std::holds_alternative<Key>(bind)) {
+			Key key = std::get<Key>(bind);
+			switch (key) {
+			case KEY_W: return "W";
+			case KEY_A: return "A";
+			case KEY_S: return "S";
+			case KEY_D: return "D";
+			case KEY_LEFT_SHIFT: return "Shift";
+			case KEY_LEFT_CONTROL: return "Ctrl";
+			case KEY_SPACE: return "Space";
+			default: return "Key " + std::to_string((int)key);
+			}
+		}
+		else {
+			int button = std::get<int>(bind);
+			switch (button) {
+			case GLFW_MOUSE_BUTTON_LEFT: return "Mouse Left";
+			case GLFW_MOUSE_BUTTON_RIGHT: return "Mouse Right";
+			default: return "Mouse " + std::to_string(button);
+			}
+		}
+	}
 };
 
 #endif
