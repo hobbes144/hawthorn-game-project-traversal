8c01a0d96891922a5d252cac72d526370bca522b
hobbes144 <77174374+hobbes144@users.noreply.github.com>
2025-04-18 19:52:34 -0700

    Prevented Respawning on Hard

--- AnishCS529Engine/AnishCS529Engine/src/FirstPersonControllerComponent.cpp
+++ AnishCS529Engine/AnishCS529Engine/src/FirstPersonControllerComponent.cpp
@@ -618,7 +618,7 @@ void FirstPersonControllerComponent::update(float deltaTime)
 	UpdateAnchorInfo();
 
 	//Check if the player is Respawning
-	if (isRespawning) {
+	if (isRespawning && difficulty != HARD) {
 		respawnPlayer(true,false);
 	}
 
