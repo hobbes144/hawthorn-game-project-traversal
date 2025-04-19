6c8b23eacd9032e451e4a50970d7683e0459d742
Anish Murthy <anish.murthy@digipen.edu>
2025-04-18 20:52:09 -0700

    Added copyright info to shaders

--- AnishCS529Engine/AnishCS529Engine/src/GlobalVariables.h
+++ AnishCS529Engine/AnishCS529Engine/src/GlobalVariables.h
@@ -1,3 +1,16 @@
+/*!****************************************************************************
+ * \file   GlobalVariables.h
+ * \author Anish Murthy (anish.murthy.dev@gmail.com)
+ * \par    **DigiPen Email**
+ *    anish.murthy@digipen.edu
+ * \par    **Course**
+ *    GAM541
+ * \date   04-18-2025
+ * \brief  Global Variables for easy use while not fully implemented
+ * 
+ * Copyright © 2025 DIGIPEN Institute of Technology. All rights reserved.
+ *
+ *****************************************************************************/
 #ifndef GLOBAL_VARIABLES_H
 #define GLOBAL_VARIABLES_H
 
